baa96953867e364c5a568bd22945f93b657e253d
asenAce <asen.lozanov.developer@gmail.com>
2015-12-12 23:35:15 +0100

    Exams Practices
    OOP Course

---
+++ Software_University_Bulgaria/Fundamental_Level/Object_Oriented_Programming/Exam_preparation/OOP_Sample_Exam_partTwo/TaskOne/01. Cosmetics Shop_frame/Cosmetics/Engine/CosmeticsFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Cosmetics.Engine
+{
+    using System.Collections.Generic;
+    using Common;
+    using Contracts;
+
+    public class CosmeticsFactory : ICosmeticsFactory
+    {
+        public ICategory CreateCategory(string name)
+        {
+            // TODO: create category
+        }
+
+        public IShampoo CreateShampoo(string name, string brand, decimal price, GenderType gender, uint milliliters, UsageType usage)
+        {
+            // TODO: create shampoo
+        }
+
+        public IToothpaste CreateToothpaste(string name, string brand, decimal price, GenderType gender, IList<string> ingredients)
+        {
+            // TODO: create toothpaste
+        }
+
+        public IShoppingCart ShoppingCart()
+        {
+            // TODO: create shopping cart
+        }
+    }
+}
---
+++ Software_University_Bulgaria/Fundamental_Level/Object_Oriented_Programming/Exam_preparation/OOP_Sample_Exam_partTwo/TaskOne/01. Cosmetics Shop_frame/Cosmetics/Products/DeleteMe.cs
@@ -0,0 +1,7 @@
+﻿namespace Cosmetics.Products
+{
+    public class DoNotDeleteMe
+    {
+        // Put all your classes in this namespace and delete this class...
+    }
+}
