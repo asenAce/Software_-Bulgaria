1432df3993abec8dfa19c25498121e5a08e07236
asenAce <asen.lozanov.developer@gmail.com>
2016-01-10 11:53:31 +0100

    1.7.StudentsScore
    C# Simple project

---
+++ Inter-Active_On-Line_Courses/Kharkov_Technical_University/1_Simple_Projects/1.7.StudentsScore/ThrowAwayGradeBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class ThrowAwayGradeBook : GradeBook
+    {
+        public ThrowAwayGradeBook(string name)  // ctor
+                :base(name)
+        {
+            Console.WriteLine("throwaway ctor");
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+
+            Console.WriteLine("Throw away");
+            float lowest = float.MaxValue;
+            foreach (float grade in grades)
+            {
+                lowest = Math.Min(lowest,grade);
+            }
+
+            grades.Remove(lowest);
+            return base.ComputeStatistics();
+        }
+
+
+    }
+}
