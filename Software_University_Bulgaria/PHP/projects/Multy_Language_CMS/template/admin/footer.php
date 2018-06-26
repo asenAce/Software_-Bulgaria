2590623447e5d3b36fc213cd4949b646834eda6d
Asen Petrow <asen.lozanov.developer@gmail.com>
2018-06-26 13:43:20 +0300

    multy-Lenguage_CMS
    DigitalInovation Varna ltd.

---
+++ Software_University_Bulgaria/PHP/projects/Multy_Language_CMS/template/admin/footer.php
@@ -0,0 +1,17 @@
+</div>
+</div>
+<footer>
+	<div id="footer">
+		<div id="footer-content">
+			<p>&copy; <a href="/" title="DigitalInovation Varna ltd.">DigitalInovation Varna ltd.</a> <?php echo date('Y'); ?></p>
+		</div>
+	</div>
+</footer>
+</div>
+<script src="/js/jquery-1.6.4.min.js"></script>
+<script src="/js/jquery.cookie.js"></script>
+<?php echo $this->script; ?>
+<script src="/js/language.js"></script>
+<script src="/admin/js/core.js"></script>
+</body>
+</html>
